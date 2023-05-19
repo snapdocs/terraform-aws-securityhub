5ff11c92e715ce1b827d34bdbf963624c84bd674
connor-snapdocs <connor.sweeney@snapdocs.com>
2023-05-19 16:33:19 -0400

    chore: remove twistlock reference

--- main.tf
+++ main.tf
@@ -11,10 +11,3 @@ resource "aws_securityhub_account" "securityhub" {}
 # https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-integrations-managing.html
 
 data "aws_region" "current" {}
-
-# https://www.terraform.io/docs/providers/aws/r/securityhub_product_subscription.html
-# Twistlock aka Prisma Cloud integration for security hub
-resource "aws_securityhub_product_subscription" "twistlock" {
-  depends_on  = [aws_securityhub_account.securityhub]
-  product_arn = "arn:aws:securityhub:${data.aws_region.current.name}:496947949261:product/twistlock/twistlock-enterprise"
-}
